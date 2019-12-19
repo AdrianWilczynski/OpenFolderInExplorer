277663803a678a3ed902302303077e4c009872c4
AdrianWilczynski <adrian.k.wilczynski@gmail.com>
2019-12-19 21:57:21 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# open-folder-in-explorer
---
+++ src/extension.ts
@@ -0,0 +1,22 @@
+import * as vscode from 'vscode';
+import * as cp from 'child_process';
+import * as os from 'os';
+
+export function activate(context: vscode.ExtensionContext) {
+	context.subscriptions.push(vscode.commands.registerCommand('extension.openFolderInExplorer', (uri: vscode.Uri) => {
+		const platform = os.platform();
+
+		const command = platform === 'win32' && 'explorer'
+			|| platform === 'linux' && 'xdg-open'
+			|| platform === 'darwin' && 'open';
+
+		if (!command) {
+			vscode.window.showWarningMessage(`Your operating system (${platform}) isn't supported.`);
+			return;
+		}
+
+		cp.execSync(`${command} "${uri.fsPath}"`);
+	}));
+}
+
+export function deactivate() { }
