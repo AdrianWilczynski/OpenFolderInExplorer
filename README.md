bb0b90b13015a60f8e458b544f313264698445ee
AdrianWilczynski <adrian.k.wilczynski@gmail.com>
2020-03-07 23:46:53 +0100

    Add optional "Open in New Instance" command

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # Open Folder in Explorer
 
-Open selected folder in explorer from context menu.
+Open selected folder in explorer (or new instance) from context menu.
 
 ![In Action](img/inAction.gif)
+
+## Configuration
+
+`openFolderInExplorer.enableOpenFolderInNewInstance` - Add "Open *Folder* in New *Visual Studio Code* Instance" to Explorer's context menu.
--- src/extension.ts
+++ src/extension.ts
@@ -3,20 +3,24 @@ import * as cp from 'child_process';
 import * as os from 'os';
 
 export function activate(context: vscode.ExtensionContext) {
-	context.subscriptions.push(vscode.commands.registerCommand('extension.openFolderInExplorer', (uri: vscode.Uri) => {
-		const platform = os.platform();
+	context.subscriptions.push(
+		vscode.commands.registerCommand('extension.openFolderInExplorer', (uri: vscode.Uri) => {
+			const platform = os.platform();
 
-		const command = (platform === 'win32' && 'explorer')
-			|| (platform === 'linux' && 'xdg-open')
-			|| (platform === 'darwin' && 'open');
+			const command = (platform === 'win32' && 'explorer')
+				|| (platform === 'linux' && 'xdg-open')
+				|| (platform === 'darwin' && 'open');
 
-		if (!command) {
-			vscode.window.showWarningMessage(`Your operating system (${platform}) isn't supported.`);
-			return;
-		}
+			if (!command) {
+				vscode.window.showWarningMessage(`Your operating system (${platform}) isn't supported.`);
+				return;
+			}
 
-		cp.execSync(`${command} "${uri.fsPath}"`);
-	}));
+			cp.execSync(`${command} "${uri.fsPath}"`);
+		}),
+		vscode.commands.registerCommand('extension.openFolderInNewInstance', (uri: vscode.Uri) => {
+			vscode.commands.executeCommand("vscode.openFolder", uri, true);
+		}));
 }
 
 export function deactivate() { }
